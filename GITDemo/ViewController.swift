e93db25ba6072f6bdba19093bd25bb8db4ca67b4
Ivan Shturkhal <ivshtur@icloud.com>
2023-08-29 15:07:03 +0300

    change background color

--- GITDemo/ViewController.swift
+++ GITDemo/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.view.backgroundColor = .blue
         setLabel(label: myLabel)
         // Do any additional setup after loading the view.
     }
