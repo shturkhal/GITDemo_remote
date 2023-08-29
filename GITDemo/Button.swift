eac58a46e3cdcd94f11270e825fd4c903f4e91cb
Ivan Shturkhal <ivshtur@icloud.com>
2023-08-29 14:53:37 +0300

    create button

---
+++ GITDemo/Button.swift
@@ -0,0 +1,20 @@
+//
+//  Button.swift
+//  GITDemo
+//
+//  Created by Іван Штурхаль on 29.08.2023.
+//
+
+import SwiftUI
+
+struct Button: View {
+    var body: some View {
+        Text("Hello, ladies!")
+    }
+}
+
+struct Button_Previews: PreviewProvider {
+    static var previews: some View {
+        Button()
+    }
+}
