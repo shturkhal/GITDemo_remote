73de14db11b9aa6617b54011d8489bf7ed71a4c3
Ivan Shturkhal <ivshtur@icloud.com>
2023-08-29 15:13:04 +0300

    add button

--- GITDemo/ViewController.swift
+++ GITDemo/ViewController.swift
@@ -12,10 +12,12 @@ class ViewController: UIViewController {
     // MARK: Properties
     
     var myLabel = UILabel(frame: CGRect(x: 150, y: 150, width: 70, height: 70))
+    var button = UIButton(frame: CGRect(x: 160, y: 160, width: 80, height: 80))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .blue
+        self.view.addSubview(button)
         setLabel(label: myLabel)
         // Do any additional setup after loading the view.
     }
